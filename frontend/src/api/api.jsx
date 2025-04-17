5d00e94ca3c021bd59a709ec7b1ecd5c7be78c0d
mittalsunit <sunitmittal01@gmail.com>
2025-04-18 00:47:39 +0530

    hosted

--- frontend/src/api/api.jsx
+++ frontend/src/api/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://spotdraft-1x72.onrender.com",
 });
 
 // Add request interceptor for tokens
